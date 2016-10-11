b3f2792e9dc4c47347a2491bb99f3679d90704d4
Bryn Walker <brynwalker1@gmail.com>
2016-10-11 13:04:27 +1100

    NEW: Add unit tests for BattleShipsGame class

---
+++ BattleShipsTests1/BattleShipsGameUnitTests.cs
@@ -0,0 +1,62 @@
+﻿using BattleShips;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleShips.Tests
+{
+    [TestClass()]
+    public class BattleShipsGameUnitTests
+    {
+        private BattleShipsGame game;
+        private Player player;
+        private AIEasyPlayer ai; 
+
+        [TestInitialize()]
+        public void Setup()
+        {
+            game = new BattleShipsGame();
+        }
+
+        [TestMethod()]
+        public void InitialisationTest()
+        {
+            // arrange
+            ai = new AIEasyPlayer(game);
+
+            // act
+            var expected = ai.Game;
+            var actual = game;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PlayerLimitTest()
+        {
+            // arrange is taken care of in Setup() method
+
+            // act
+            int expected = 2;
+            int actual = game.Players.Length;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ApplicationException), "You cannot add another player, the game already has two players.")]
+        public void AddPlayerLimitTest()
+        {
+            // arrange
+            Player player1;
+            Player player2;
+            Player player3;
+
+            // act
+            player1 = new Player(game);
+            player2 = new Player(game);
+            player3 = new Player(game);
+        }
+    }
+}
