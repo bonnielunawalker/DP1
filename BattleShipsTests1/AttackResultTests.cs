5fb5d6f66eae3e4d6925e7a242192a38b44b234c
CrossyX <5840422@student.swin.edu.au>
2016-10-21 17:04:38 +1100

    Add files via upload
    NEW - Unit test to check attack result

---
+++ BattleShipsTests1/AttackResultTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class AttackResultTests
+    {
+        [TestMethod()]
+		public void AttackResultToString ()
+		{
+			AttackResult attackResult = new AttackResult (ResultOfAttack.GameOver, "Game over", 0, 0);
+
+			string actual = attackResult.ToString ();
+			string expected = "Game over";
+			Assert.AreEqual (expected, actual, "Error: AttackResultToString");
+		}
+
+		[TestMethod()]
+		public void AttackResultToStringWithShip ()
+		{
+			Ship ship = new Ship (ShipName.AircraftCarrier);
+			AttackResult attackResult = new AttackResult (ResultOfAttack.GameOver, ship, "Game over", 0, 0);
+
+			string actual = attackResult.ToString ();
+			string expected = "Game over " + ship.Name;
+
+			Assert.AreEqual (expected, actual, "Error: AttackResultToStringWithShip");
+		}
+    }
+}
